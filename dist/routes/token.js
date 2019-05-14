27864a4adc4452560741d2642bcd8f5e622c7b98
Nick Parsons <nparsons08@gmail.com>
2019-05-14 15:42:56 -0600

    Remove dist

--- dist/routes/token.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _token = require("../controllers/v1/token");
-
-var _controllers = require("../utils/controllers");
-
-module.exports = api => {
-  api.route("/v1/token").post((0, _controllers.wrapAsync)(_token.token));
-};
